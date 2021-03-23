ca716ee13f202307239962f5c8c64f70ef0140c0
jon4h <admin@psytux.com>
2021-03-24 00:09:49 +0100

    testing

--- webhook-cd.py
+++ webhook-cd.py
@@ -8,17 +8,23 @@
 from hashlib import sha1
 import hmac
 
-from flask import Flask, request, abort
+from flask import Flask, request
 
 app = Flask(__name__)
 
+def verify_signature(req):
+    received_sign = req.headers.get('X-Hub-Signature').split('sha1=')[-1].strip()
+    secret = 'my_secret_string'.encode()
+    expected_sign = hmac.HMAC(key=secret, msg=req.data, digestmod=sha1).hexdigest()
+    return hmac.compare_digest(received_sign, expected_sign)
+
+
 @app.route('/webhooks/containers', methods=['POST'])
-def index():
-    request_signature = request.headers.get('X-Hub-Signature')
-
-    import config
-
-    computed_signature = 'sha1' + hmac.new(config.WEBHOOK_SECRET,msg=request.data, digestmod=sha1)
-
-    if not hmac.compare_digest(computed_signature.hexdigest(), request_signature):
-        abort(500)
+def main():
+    if request.method == 'POST':
+        if verify_signature(request):
+            import config
+            print(config.DOCKER_CONTAINERS)
+            return 'Success', 200
+        return 'Forbidden', 403
+    return 'Not allowed', 405
