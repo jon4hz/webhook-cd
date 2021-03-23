d2a147663c452c59aff41dbdeca3b94686d74e4f
jon4h <admin@psytux.com>
2021-03-24 00:18:38 +0100

    import error to exit

--- webhook-cd.py
+++ webhook-cd.py
@@ -9,10 +9,12 @@ from hashlib import sha1
 import hmac
 
 from flask import Flask, request
+from waitress import serve
 
 app = Flask(__name__)
 
-def verify_signature(req):
+
+def verify_signature(req, secret):
     received_sign = req.headers.get('X-Hub-Signature').split('sha1=')[-1].strip()
     secret = 'my_secret_string'.encode()
     expected_sign = hmac.HMAC(key=secret, msg=req.data, digestmod=sha1).hexdigest()
@@ -22,9 +24,15 @@ def verify_signature(req):
 @app.route('/webhooks/containers', methods=['POST'])
 def main():
     if request.method == 'POST':
-        if verify_signature(request):
+        try:
             import config
+        except ImportError:
+            exit()
+        if verify_signature(request, config.WEBHOOK_SECRET):
             print(config.DOCKER_CONTAINERS)
             return 'Success', 200
         return 'Forbidden', 403
     return 'Not allowed', 405
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=80)
