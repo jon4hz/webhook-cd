84cd997890ef7a917d6c523f173d23b809abc2de
jon4h <admin@psytux.com>
2021-03-23 23:57:57 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Webhook-cd
+## About
+This is a simple webhook server, written in python. Once the webhook is triggered (from a GitHub Action for example), the script automatically updates predefined containers using [watchtower](https://containrrr.dev/watchtower).
+
+## How to
+\- More notes than instructions \-
+* Create venv
+* Create systemd service
+* Create config file
+* Enable & start systemd service
---
+++ webhook-cd.py
@@ -0,0 +1,24 @@
+#!./venv/bin/python
+#
+# Author: jon4hz
+# Date: 24.03.2021
+# Desc: Webhook for cd
+###################################################################################################
+
+from hashlib import sha1
+import hmac
+
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+@app.route('/webhooks/optimizely', methods=['POST'])
+def index():
+    request_signature = request.headers.get('X-Hub-Signature')
+
+    import config
+
+    computed_signature = 'sha1' + hmac.new(config.WEBHOOK_SECRET,msg=request.data, digestmod=sha1)
+
+    if not hmac.compare_digest(computed_signature.hexdigest(), request_signature):
+        abort(500)
