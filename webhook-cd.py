b225f0820214dba3e557884db521b1405f1db01f
jon4h <admin@psytux.com>
2021-03-24 00:32:44 +0100

    change default port

--- webhook-cd.py
+++ webhook-cd.py
@@ -34,5 +34,6 @@ def main():
         return 'Forbidden', 403
     return 'Not allowed', 405
 
+
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=80)
+    serve(app, host='0.0.0.0', port=8888)
